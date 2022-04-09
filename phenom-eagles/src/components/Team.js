92e5b06371b180b6b054c392d976617cf3bd87b1
iandominic <javalavama@gmail.com>
2022-04-09 17:36:31 +0800

    icon

--- phenom-eagles/src/components/Team.js
+++ phenom-eagles/src/components/Team.js
@@ -25,15 +25,17 @@ const Team = () => {
             Team
         </h1>
         <div className="team__container">
-            <MemberComponent img={img1} name="$amurai" position="Developer" />
-            <MemberComponent img={img2} name="Cinimod" position="Developer" />
-            <MemberComponent img={img3} name="KymT" position="Tite" />
-            <MemberComponent img={img4} name="Phenom Eagle" position="Founder" />
-            <MemberComponent img={img5} name="Phenom Eagle" position="Founder" />
-            <MemberComponent img={img6} name="Phenom Eagle" position="Founder" />
-            <MemberComponent img={img2} name="Phenom Eagle" position="Founder" />
-            <MemberComponent img={img5} name="Phenom Eagle" position="Founder" />
-            <MemberComponent img={img1} name="Phenom Eagle" position="Founder" />
+            <MemberComponent img={img1} name="Clone" position="Founder" />
+            <MemberComponent img={img2} name="Sierramadre" position="Co-Founder" />
+            <MemberComponent img={img3} name="Fang" position="NFT Advisor" />
+            <MemberComponent img={img4} name="Duckey" position="Business Developer" />
+            <MemberComponent img={img5} name="CHRI$TIAN" position="Blockchain Developer" />
+            <MemberComponent img={img6} name="Kymt" position="Co-Developer" />
+            <MemberComponent img={img2} name="FUNGUY07" position="Social Media Manager" />
+            <MemberComponent img={img5} name="Cinimod" position="Web Developer" />
+            <MemberComponent img={img1} name="JERAX" position="Artist/Graphic Design" />
+            <MemberComponent img={img3} name="AAARON" position="Artist/Graphic Design" />
+            <MemberComponent img={img5} name="ENZ" position="Graphic Design" />
         </div>
     </section>
   )
